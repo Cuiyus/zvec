c4dd8319d1e96f8b33cf5570869651aec90e7a6a
叁弌 <cuiyushuai.cys@alibaba-inc.com>
2026-06-03 10:34:00 +0800

    fix: remove unused Doc import in rerank_function.py (ruff F401)

--- python/zvec/extension/rerank_function.py
+++ python/zvec/extension/rerank_function.py
@@ -16,7 +16,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from ..model.doc import Doc, QueryResult
+from ..model.doc import QueryResult
 
 
 class RerankFunction(ABC):
